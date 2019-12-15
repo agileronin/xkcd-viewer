8923afcf9a6e3888d4e41bcc506bdfd7daeaaec8
Gregory Mundy <gregmundy@gmail.com>
2019-12-15 02:17:08 -0500

    style: cleanup CartoonViewer container

--- src/components/CartoonViewer.js
+++ src/components/CartoonViewer.js
@@ -4,13 +4,13 @@ import Cartoon from './Cartoon';
 import ControlPanel from './ControlPanel';
 
 const CartoonViewer = () => {
-    return(
-        <div className="ui container segment">
-            <Cartoon />
-<           div className="ui divider"></div>
-            <ControlPanel />
-        </div>
-    );
-}
+  return (
+    <div className="ui container segment">
+      <Cartoon />
+      <div className="ui divider"></div>
+      <ControlPanel />
+    </div>
+  );
+};
 
 export default CartoonViewer;
